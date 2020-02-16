92e119a78480dd98658578015f97b7f3158511b4
Charissa Beaty <char.beaty11@gmail.com>
2020-02-15 23:05:25 -0700

    added images and files

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="copyright col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 center">
+                <small className='footertext'>Charissa Beaty </small><small className='footertext'>&copy;2020</small>
+            </div>
+        </div>
+    )
+}
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Jumbotronhome from '../components/JumbotronHome';
+import Footer from '../components/Footer';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Jumbotronhome />
+            <Footer />
+
+        </div>
+
+    );
+}
+
+export default Home; 
